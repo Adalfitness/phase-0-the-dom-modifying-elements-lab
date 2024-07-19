748c218d72b9ea9aea30e69fa9eacfbc5d354450
carlos bernal <bernal199152@yahoo.com>
2024-07-19 15:40:13 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainElement = document.querySelector('#main');
+mainElement.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.textContent = "Carlos is the champion";
+newHeader.id = "victory";
